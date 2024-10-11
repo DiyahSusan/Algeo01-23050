0b7f7e19b2fe89224e7b073463e2adcf6a0105f9
maymilla <maylayaffa12@gmail.com>
2024-10-11 22:20:42 +0700

    fix: invers

--- src/Function/Invers.java
+++ src/Function/Invers.java
@@ -6,11 +6,12 @@ public class Invers{
     // belum tau kalau matriksnya gak invertible, ngembaliin apa
     public static Matrix Invers(Matrix m){
         if (m.isSquare()){
-            Matrix hasil = this;
-            Matrix identitas;
+            Matrix matriks = new Matrix(m.row, m.col);
+            Matrix identitas = new Matrix(m.row, m.col);
+
+            matriks.matrix = m.matrix;
             
-            // membuat matriks identitas
-            identitas.createMatrix(m.row, m.col);
+            // mengisi matriks identitas
 
             int i = 0, j = 0, k;
             for (i = 0; i < m.row; i++){
@@ -22,40 +23,40 @@ public class Invers{
                     }
                 }
             }
-            
+        
             i = 0; 
             j = 0;
             //melakukan eliminasi gauss jordan pada matrix awal dan melakukan langkah yang sama pada matrix identitas
-            while(i<this.row && j < this.col){
+            while(i<matriks.row && j < matriks.col){
 
                 k = i+1;
-                while(hasil.matrix[i][j] == 0 && k < this.row){
-                    hasil.rowSwap(i, k);
+                while(matriks.matrix[i][j] == 0 && k < matriks.row){
+                    matriks.rowSwap(i, k);
                     identitas.rowSwap(i,k);
                     k+=1;
                 }
 
-                if(hasil.matrix[i][j] == 0){
+                if(matriks.matrix[i][j] == 0){
                     j+=1;
                     continue;
                 }
 
-                double pembagi = 1/hasil.matrix[i][j];
-                hasil.multiplyRow(i, pembagi);
+                double pembagi = 1/matriks.matrix[i][j];
+                matriks.multiplyRow(i, pembagi);
                 identitas.multiplyRow(i, pembagi);
 
                 k = i+1;
-                while(k<hasil.row){
+                while(k<matriks.row){
 
-                    hasil.sumMultiplyRow(k, i, (-1) * hasil.matrix[k][j]);
-                    identitas.sumMultiplyRow(k, i, (-1) * identitas.matrix[k][j]);
+                    matriks.sumMultiplyRow(k, i, -matriks.matrix[k][j]);
+                    identitas.sumMultiplyRow(k, i, -identitas.matrix[k][j]);
 
                     k+=1;
                 }
 
                 k = i-1;
                 while (k >= 0) {
-                    hasil.sumMultiplyRow(k, i, -hasil.matrix[k][j]);
+                    matriks.sumMultiplyRow(k, i, -matriks.matrix[k][j]);
                     identitas.sumMultiplyRow(k, i, -identitas.matrix[k][j]);
 
                     k -= 1;
