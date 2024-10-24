9d5b8605cb3d6191e930a97e6fc261d7b8271edd
unknown <izzatjundy04@gmail.com>
2024-10-24 16:32:27 +0700

    fix: isNoSolution

--- src/Function/RegresiLinear.java
+++ src/Function/RegresiLinear.java
@@ -195,9 +195,6 @@ public class RegresiLinear{
             return; // Keluar jika ada kesalahan
         }
 
-<<<<<<< HEAD
-        // Membuat matriks yang memisahkan variabel X dan hasil Y
-=======
         //matrix variabel independennya aja
         Matrix mNew = new Matrix();
         mNew.createMatrix(m1.row - 1, m1.col);
@@ -207,7 +204,6 @@ public class RegresiLinear{
             }
         }
         // nilai yang ingin ditaksir
->>>>>>> 59932085fcc3f5d1cfa021c2b6fe30a5ad053336
         m2 = new double[m1.col - 1];
         for(i = 0; i < m2.length; i++){ 
             m2[i] = m1.getElement(m1.row - 1, i);
@@ -215,13 +211,8 @@ public class RegresiLinear{
 
         // membuat matriks baru untuk SPL
         mTemp = new Matrix();
-<<<<<<< HEAD
-        mTemp.createMatrix(m1.col, m1.col + 1);
-        mTemp = createSPLMatrix(m1); // Menggunakan matriks m1
-=======
         mTemp.createMatrix(mNew.col, mNew.col + 1);
         mTemp = createSPLMatrix(mNew); // Menggunakan matriks m1
->>>>>>> 59932085fcc3f5d1cfa021c2b6fe30a5ad053336
 
         // melakukan eliminasi Gauss
         mTemp = mTemp.gaussElimination();
@@ -324,181 +315,6 @@ public class RegresiLinear{
     }
 
 
-<<<<<<< HEAD
-    // public static void regresiLinearFile (Matrix m){
-    //     Scanner scanner = new Scanner(System.in);
-    //     int i, j;
-    //     double result = 0;
-    //     double sum = 0;
-    //     Matrix mTemp;
-    //     Matrix m1;
-    //     double[] m2;
-    //     double[] x;
-    //     BufferedReader inputFile = new BufferedReader(new InputStreamReader(System.in));
-
-    //     //membaca file 
-    //     String fileName = "";
-    //     System.out.println("Masukkan nama file untuk membaca data: ");
-    //     try {
-    //         fileName = inputFile.readLine();
-    //         // Membaca data dari file
-    //         FileReader fr = new FileReader("test/Input/" + fileName);
-    //         BufferedReader br = new BufferedReader(fr);
-            
-    //         // Membaca jumlah baris dan kolom dari file
-    //         int rowCount = 0;
-    //         String line;
-    //         while ((line = br.readLine()) != null) {
-    //             rowCount++;
-    //         }
-
-    //         // Mengatur ulang reader untuk membaca dari awal lagi
-    //         br.close();
-    //         fr = new FileReader("test/Input/" + fileName);
-    //         br = new BufferedReader(fr);
-
-    //         // membuat matriks dengan ukuran yang sesuai
-    //         m1 = new Matrix();
-    //         m1.createMatrix(rowCount, m.col); // Menggunakan jumlah baris yang dibaca dari file
-
-    //         // membaca isi file ke dalam matriks m1
-    //         int currentRow = 0;
-    //         while ((line = br.readLine()) != null) {
-    //             String[] values = line.split("\\s+"); // Misalnya, nilai dipisahkan oleh spasi
-    //             for (j = 0; j < values.length; j++) {
-    //                 m1.setElement(currentRow, j, Double.parseDouble(values[j]));
-    //             }
-    //             currentRow++;
-    //         }
-    //         br.close();
-    //     } catch (IOException err) {
-    //         err.printStackTrace();
-    //         return; // Keluar jika ada kesalahan
-    //     }
-        
-
-    //     // membuat matriks dari file yang diinput
-    //     m1 = new Matrix();
-    //     m1.createMatrix(m.row - 1, m.col);
-    //     for(i = 0; i < m.row - 1; i++){
-    //         for(j = 0; j < m.col; j++){
-    //             m1.setElement(i, j, m.matrix[i][j]);
-    //         }
-    //     }
-
-    //     m2 = new double[m.col - 1];
-    //     for(i = 0; i < m2.length; i++){ 
-    //         m2[i] = m.getElement(m.row - 1, i);
-    //     }
-
-    //     //membuat matriks baru semacam SPL
-    //     mTemp = new Matrix();
-    //     mTemp.createMatrix(m.col, m.col + 1);
-
-    //     mTemp = createSPLMatrix(m);
-        
-    //     // melakukan eliminasi Gauss
-    //     mTemp = mTemp.gaussElimination();
-    //     x = new double [mTemp.getRowLength()];
-    //     Matrix.backSubstitution(mTemp, x); 
-
-    //     System.out.print("f(x) = ");
-    //     for (i = 0; i < mTemp.row; i++) {
-    //         if (i == 0){
-    //             result = x[i];
-    //             if (x[i] > 0){
-    //                 System.out.printf("%.4f ", x[i]);
-    //             } else {
-    //                 x[i] *= -1;
-    //                 System.out.printf("- %.4f ", x[i]);
-    //             }
-    //         } else if ( i > 0 && i < mTemp.row - 1){
-    //             result = x[i] * m2[i - 1];
-    //             if (x[i] > 0){
-    //                 System.out.printf("+ %.4fx%d ", x[i], i);
-    //             } else {
-    //                 x[i] *= -1;
-    //                 System.out.printf("- %.4fx%d ", x[i], i);
-    //             }
-    //         } else if (i == mTemp.row - 1){
-    //             result = x[i] * m2[i - 1];
-    //             if (x[i] > 0){
-    //                 System.out.printf("+ %.4fx%d, ", x[i], i);
-    //             } else {
-    //                 x[i] *= -1;
-    //                 System.out.printf("- %.4fx%d, ", x[i], i);
-    //             }
-    //         }
-    //         sum += result;
-    //     }
-    //     System.out.printf("f(xk) = %.4f\n", sum);
-         
-    //     //Output
-    //     int opsi = Output.opsiOutput();
-    //     if (opsi == 1) {
-    //         // mencetak output ke dalam bentuk file
-    //         String nameFile = "";
-    //         System.out.println("Masukkan nama file: ");
-    //         try {
-    //             nameFile = inputFile.readLine();
-    //             String path = "test/Output/" + nameFile;
-
-    //             // cek filenya udah ada belum
-    //             File file = new File(path);
-    //             if (file.exists()) {
-    //                 System.out.println("File sudah ada. Apakah Anda ingin menimpanya? (y/n)");
-    //                 char choice = scanner.next().charAt(0);
-    //                 if (choice != 'y' && choice != 'Y') {
-    //                     System.out.println("Output dibatalkan.");
-    //                     return; //kalau ngga mau jadiin file berarti batal
-    //                 }
-    //             }
-    //         } catch (IOException err) {
-    //             err.printStackTrace();
-    //         }
-
-    //         try {
-    //             FileWriter file = new FileWriter("test/Output/" + nameFile);
-    //             file.write("f(x) = ");
-    //             for (i = 0; i < mTemp.row; i++) {
-    //                 if (i == 0){
-    //                     result = x[i];
-    //                     if (x[i] > 0){
-    //                         file.write(String.format("%.4f", x[i]));
-    //                     } else {
-    //                         x[i] *= -1;
-    //                         file.write(" - "+String.format("%.4f", x[i]));
-    //                     }
-    //                 } else if (i > 0 && i < mTemp.row - 1){
-    //                     result = x[i] * m2[i - 1];
-    //                     if (x[i] > 0){
-    //                         file.write(" + "+ String.format("%.4f", x[i]) +"x"+Integer.toString(i));
-    //                     } else {
-    //                         x[i] *= -1;
-    //                         file.write(" - "+ String.format("%.4f", x[i]) +"x"+Integer.toString(i));
-    //                     }
-    //                 } else if (i == mTemp.row - 1){
-    //                     result = x[i] * m2[i - 1];
-    //                     if (x[i] > 0){
-    //                         file.write(" + "+String.format("%.4f", x[i]) +"x"+Integer.toString(i));
-    //                     } else {
-    //                         x[i] *= -1;
-    //                         file.write(" - "+String.format("%.4f", x[i]) +"x"+Integer.toString(i));
-    //                     }
-    //                 }
-    //                 sum += result;
-    //             }
-    //             file.write("\nf(xk) = "+String.format("%.4f", sum));
-    //             file.close();
-    //         } catch (IOException err) {
-    //             err.printStackTrace();
-    //         }
-    //     }
-        
-    // }
-
-=======
->>>>>>> 59932085fcc3f5d1cfa021c2b6fe30a5ad053336
     //convert matrix ke bentuk SPL
     private static Matrix createSPLMatrix(Matrix m) {
         Matrix mTemp = new Matrix();
